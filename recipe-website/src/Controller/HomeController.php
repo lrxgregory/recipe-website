bd15575279358cffeecbc9166ebecc3783a8d2d6
lrxgregory <lrxgregory@gmail.com>
2024-09-11 18:36:31 +0200

    fix homepage + remove locale

--- recipe-website/src/Controller/HomeController.php
+++ recipe-website/src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,8 +11,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(RecipeRepository $repository, Request $request): Response
     {
-        return $this->render('home/index.html.twig');
+        $page = $request->query->getInt('page', 1);
+
+        $recipes = $repository->paginateRecipes($page);
+
+        return $this->render('home/index.html.twig', [
+            'recipes' => $recipes,
+        ]);
     }
 }
